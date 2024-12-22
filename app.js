b9301b7f14f7b03d0462f1712d954a9e61eb1c97
siddharth shukla <siddsanskshukla@gmail.com>
2024-12-22 14:48:58 +0530

    crud operation perform

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const path=require("path");
 const app=express();
+const methodOverride=require("method-override") 
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js")
+
+app.use(methodOverride("_method"));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -25,11 +28,40 @@ app.get('/listings',async (req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings})
 })
-
+//New route
+app.get('/listings/new',(req,res)=>{
+    res.render("listings/new.ejs")
+})
+//Show route
 app.get('/listings/:id',async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+})
+//create route
+app.post('/listings',async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+})
+//edit route
+app.get('/listings/:id/edit',async (req,res)=>{
+    let { id }=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+})
+//update route
+app.put('/listings/:id',async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`)
+})
+
+//delete route
+app.delete('/listings/:id',async (req,res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findByIdAndDelete(id);
+    res.redirect("/listings")
 })
 
 // app.get('/',(req,res)=>{
