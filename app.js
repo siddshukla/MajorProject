83070952e190ba12568933e26685289bab0c1db9
siddharth shukla <siddsanskshukla@gmail.com>
2024-12-22 17:19:41 +0530

    adding styling (navbar and footer)

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app=express();
 const methodOverride=require("method-override") 
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js")
+const ejsMate=require('ejs-mate');
 
 app.use(methodOverride("_method"));
-
+app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.set("view engine","views");
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+
+app.engine("ejs",ejsMate);
+
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
@@ -23,7 +27,9 @@ main().then(()=>{
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
-
+app.get('/',(req,res)=>{
+    res.send("home Rout!")
+})
 app.get('/listings',async (req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings})
